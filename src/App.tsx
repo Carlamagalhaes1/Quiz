6903ab6594e1835ed2be83b7f1ddd23400a102b0
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-03-25 20:17:25 -0300

    estilos do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 //import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <main>
-        <header>
-          <h1>Teste suas habilidaes!</h1>
+    <main>
+      <div className='border p-4 border-[#3F5185] '>
+      <header className='text-base pt-[18px] pb-[18px] pl-[33px] pr-[33px]' >
+          <h1 >Teste suas habilidaes!</h1>
         </header>
         <p>
           Teste os seus conhecimentos sobre o universo Harry Potter e divirta-se.
         </p>
+        <p>FORMULARIO / BOTÃO</p>
         <footer>
           <p>
             Orgulhosamente criado por Carla
           </p>
         </footer>
 
+
+      </div>
+        
       </main>
+   
+    
+
      
     </>
   )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
     return(
         <div>
-            
+            <h1>lalal</h1>
+
         </div>
     )
     
