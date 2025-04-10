251473e3284145b340cc8914cc4d061939995482
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-04-10 20:26:08 -0300

    img

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ function App() {
 
   return (
     <>
-    <main>
+    <main className='img'>
+      
+      
       <div className='border p-4 border-[#740001] responsive-container w-full '>
+        
       <header className='text-base bg-primary pt-[18px] pb-[18px] pl-[33px] pr-[33px]' >
           <h1 className='text-[14px] ' >Teste suas habilidaes!</h1>
         </header>
@@ -17,14 +20,17 @@ function App() {
           Teste os seus conhecimentos sobre o universo Harry Potter e divirta-se!!!
         </p>
         <p>FORMULARIO / BOTÃO</p>
+        
         <footer>
           <p>
             Orgulhosamente criado por Carla!
           </p>
         </footer>
+        
 
 
       </div>
+     
         
       </main>
    
