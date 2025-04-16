929f537c71ff8fe3dc4c8c8dbbd904dfe8e049c6
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-04-16 11:39:52 -0300

    consertando o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,40 @@ function App() {
 
   return (
     <>
-    <main className='img'>
-      
-      
-      <div className='border p-4 border-[#740001] responsive-container w-full bg-card rounded-t-[4px] rounded-t-[4px] rounded-bl-[4px] rounded-br-[4px] '>
-        
-      <header className='text-base bg-primary pt-[18px] pb-[18px] pl-[33px] pr-[33px] ' >
-          <h1 className='text-[14px] ' >Teste suas habilidaes!</h1>
-        </header>
-        <p>
-          Teste os seus conhecimentos sobre o universo Harry Potter e divirta-se!!!
-        </p>
-        <p>FORMULARIO / BOTÃO</p>
-        
+      <main className='img'>
+
+
+        <div className='border p-4 border-[#740001] responsive-container w-full bg-card rounded-t-[4px] rounded-t-[4px] rounded-bl-[4px] rounded-br-[4px] '>
+
+          <header className='text-base bg-primary pt-[18px] pb-[18px] pl-[33px] pr-[33px] ' >
+            <h1 className='text-sm ' >Teste suas habilidaes!</h1>
+          </header>
+          <section className='py-[24px] px-[34px] text-sm'>
+            <p>
+              Teste os seus conhecimentos sobre o universo Harry Potter e divirta-se!!!
+            </p>
+            <p>FORMULARIO / BOTÃO</p>
+
+          </section>
+
+
+
+
+
+
+        </div>
         <footer>
           <p>
             Orgulhosamente criado por Carla!
           </p>
         </footer>
-        
 
 
-      </div>
-     
-        
       </main>
-   
-    
 
-     
+
+
+
     </>
   )
 }
