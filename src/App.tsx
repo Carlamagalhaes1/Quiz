8266f60b6f5524d5d58e6ed8639a26d7a8bfea15
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-05 10:26:02 -0300

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,16 @@ function App() {
   return (
     <>
       <main className='img'>
-        
-      
-
-        
+      <img src="images/logohp.png" alt="imagem logo" className='w-24 p-2  ' />
 
 
-        <div className='border border-[#740001] responsive-container w-full bg-card rounded-t-[4px]  rounded-bl-[4px] rounded-br-[4px] '>
+        <div className='border  border-[#740001] responsive-container w-full bg-card rounded-t-[4px]  rounded-bl-[4px] rounded-br-[4px] '>
 
           <header className='text-base bg-primary pt-4.5 pb-4.5 pl-8.5 pr-8.5 p-0' >
             <h1 className='text-sm font-bold ' >Teste suas habilidaes!</h1>
           </header>
           <section className='py-6 px-8.5 text-sm'>
-            <p>
+            <p className='text-sm'>
               Testes os seus conhecimentos sobre o universo Harry Potter eudivirta-se!!!
             </p>
             <p>FORMULARIO / BOTÃO</p>
