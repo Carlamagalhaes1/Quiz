544b652fe737cdc0b371f401a5e93a0480e9ddd1
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-09 19:56:18 -0300

    home, botçao feitos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,20 @@ import logohp from "../assets/logohp.png";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-purple-900 to-black flex flex-col items-center justify-center text-white p-4">
-      <img src={logohp} alt="Harry Potter Logo" className="w-48 mb-8" />
+   <div className="min-h-screen bg-gradient-to-b from-purple-900 to-black flex flex-col items-center justify-center text-white px-4 py-8">
+  <img src={logohp} alt="Harry Potter Logo" className="w-32 sm:w-48 mb-8" />
 
-      <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Quiz do Harry Potter!</h1>
-      <p className="mb-8 text-lg text-center max-w-md">
-        Teste seus conhecimentos sobre o mundo mágico de Harry Potter. Você está pronto?
-      </p>
+  <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-center">
+    Bem-vindo ao Quiz do Harry Potter!
+  </h1>
 
-      <Link to="/quiz">
-        <Button>Começar o Quiz</Button>
-      </Link>
-    </div>
+  <p className="mb-8 text-base sm:text-lg text-center max-w-md">
+    Teste seus conhecimentos sobre o mundo mágico de Harry Potter. Você está pronto?
+  </p>
+
+  <Link to="/quiz">
+    <Button>Começar o Quiz</Button>
+  </Link>
+</div>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+
 
 export const router = createBrowserRouter ([
     {
         path:'/',
         element: <Home/>
+    },
+    {
+        path:'quiz',
+        element:<Quiz/>
     }
 
 ])
