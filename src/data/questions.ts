35e2477eb045c244ccfa99d2e14d661b4c5c982c
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-11 09:30:26 -0300

    questions atualizada

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,4 +1,3 @@
-// src/data/questions.ts
 import { Question } from "../types/quiz";
 
 export const questions: Question[] = [
@@ -12,5 +11,84 @@ export const questions: Question[] = [
     ],
     correctAnswer: "Harry James Potter",
   },
-  // ... mais perguntas
+  {
+    question: "Quem é o diretor de Hogwarts?",
+    options: [
+      "Severo Snape",
+      "Minerva McGonagall",
+      "Alvo Dumbledore",
+      "Dolores Umbridge",
+    ],
+    correctAnswer: "Alvo Dumbledore",
+  },
+  {
+    question: "Qual é a casa rival da Grifinória?",
+    options: [
+      "Sonserina",
+      "Corvinal",
+      "Lufa-Lufa",
+      "Durmstrang",
+    ],
+    correctAnswer: "Sonserina",
+  },
+  {
+    question: "Qual é o nome do elfo doméstico de Harry?",
+    options: ["Dobby", "Kreacher", "Winky", "Hokey"],
+    correctAnswer: "Dobby",
+  },
+  {
+    question: "Quem matou Dumbledore?",
+    options: ["Bellatrix Lestrange", "Severo Snape", "Lord Voldemort", "Draco Malfoy"],
+    correctAnswer: "Severo Snape",
+  },
+  {
+    question: "Qual é o patrono do Harry?",
+    options: ["Cervo", "Cavalo", "Lobo", "Fênix"],
+    correctAnswer: "Cervo",
+  },
+  {
+    question: "Quem é o padrinho de Harry Potter?",
+    options: ["Remo Lupin", "Arthur Weasley", "Sirius Black", "Alastor Moody"],
+    correctAnswer: "Sirius Black",
+  },
+  {
+    question: "Qual é o nome da coruja de Harry?",
+    options: ["Edwiges", "Hermes", "Pichitinho", "Bicuço"],
+    correctAnswer: "Edwiges",
+  },
+  {
+    question: "Qual é o nome do banco dos bruxos?",
+    options: ["Gringotes", "Gringotts", "Grimmauld", "Olivanders"],
+    correctAnswer: "Gringotes",
+  },
+  {
+    question: "O que é um trasgo montanhês?",
+    options: ["Criatura mágica", "Feitiço", "Varinha", "Quadribol"],
+    correctAnswer: "Criatura mágica",
+  },
+  {
+    question: "Qual é o nome do jornal bruxo?",
+    options: ["O Profeta Diário", "A Tribuna Mágica", "Gazeta de Hogwarts", "Correio Coruja"],
+    correctAnswer: "O Profeta Diário",
+  },
+  {
+    question: "Quem fundou a Sonserina?",
+    options: ["Salazar Sonserina", "Godric Gryffindor", "Rowena Ravenclaw", "Helga Hufflepuff"],
+    correctAnswer: "Salazar Sonserina",
+  },
+  {
+    question: "Qual é o nome verdadeiro de Voldemort?",
+    options: ["Tom Marvolo Riddle", "Tom Riddle Junior", "Tom Severus Riddle", "Marvolo Gaunt"],
+    correctAnswer: "Tom Marvolo Riddle",
+  },
+  {
+    question: "Qual é o número da plataforma para embarcar no Expresso de Hogwarts?",
+    options: ["9 ¾", "10 ½", "7 ¾", "8 ½"],
+    correctAnswer: "9 ¾",
+  },
+  {
+    question: "Qual objeto é usado para guardar memórias?",
+    options: ["Pensieve", "Horcrux", "Vira-tempo", "Espelho de Ojesed"],
+    correctAnswer: "Pensieve",
+  },
 ];
