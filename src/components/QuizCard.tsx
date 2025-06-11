c76898ebe3c47cf3c991274c9b7ed3e68fa30846
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-11 12:20:15 -0300

    alinhando card na esquerda

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -2,6 +2,7 @@ import { ChevronLeft } from "lucide-react";
 import { useState } from "react";
 import { questions } from "../data/questions";
 
+
 export function QuizCard() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
@@ -14,66 +15,66 @@ export function QuizCard() {
   const handleConfirm = () => {
     if (!selectedOption) return;
 
-    // Aqui você pode salvar a resposta, validar, etc.
+    
     setSelectedOption(null);
     if (currentIndex < questions.length - 1) {
       setCurrentIndex((prev) => prev + 1);
     } else {
       alert("Quiz finalizado!");
-      
+
     }
   };
 
   return (
-    <div className="flex flex-col h-screen  ">
-      <div className="bg-indigo-700 text-white p-4 flex items-center rounded-t-4xl ">
-        <button className="mr-4">
-          <ChevronLeft size={24} />
-        </button>
-        <h1 className="text-lg font-medium ">
-          Pergunta {currentIndex + 1} de {questions.length}
-        </h1>
-      </div>
-
     
-      <div className=" rounded-b-4xl bg-gray-900 text-white p-6 flex flex-col">
-  
-        <div className="mb-2">
-          <h2 className="text-xl font-bold mb-1">{currentQuestion.question}</h2>
+      <div className=" bg-amber-200">
+        <div className="bg-indigo-700 text-white p-4 flex rounded-t-4xl ">
+          <button className="mr-4">
+            <ChevronLeft size={24} />
+          </button>
+          <h1 className="text-lg font-medium ">
+            Pergunta {currentIndex + 1} de {questions.length}
+          </h1>
         </div>
 
 
-        <div  >
-          <div className="space-y-3 mb-3 ">
-            {currentQuestion.options.map((option) => (
+        <div className=" rounded-b-4xl bg-gray-900 text-white p-6 flex flex-col">
+
+          <div className="mb-2">
+            <h2 className="text-xl font-bold mb-1">{currentQuestion.question}</h2>
+          </div>
+
+
+          <div  >
+            <div className="space-y-3 mb-3 ">
+              {currentQuestion.options.map((option) => (
+                <button
+                  key={option}
+                  onClick={() => handleOptionSelect(option)}
+                  className={`w-full p-4 text-left rounded ${selectedOption === option
+                      ? "bg-indigo-700"
+                      : "bg-gray-800 hover:bg-gray-700"
+                    }`}
+                >
+                  {option}
+                </button>
+              ))}
+
+            </div>
             <button
-              key={option}
-              onClick={() => handleOptionSelect(option)}
-              className={`w-full p-4 text-left rounded ${
-                selectedOption === option
-                  ? "bg-indigo-700"
-                  : "bg-gray-800 hover:bg-gray-700"
-              }`}
+              onClick={handleConfirm}
+              disabled={!selectedOption}
+              className={`mt-auto w-full py-3 rounded font-medium ${selectedOption
+                  ? "bg-indigo-600 text-white hover:bg-indigo-500"
+                  : "bg-gray-600 text-gray-300 cursor-not-allowed"
+                }`}
             >
-              {option}
+              CONFIRMAR
             </button>
-          ))}
 
           </div>
-           <button
-          onClick={handleConfirm}
-          disabled={!selectedOption}
-          className={`mt-auto w-full py-3 rounded font-medium ${
-            selectedOption
-              ? "bg-indigo-600 text-white hover:bg-indigo-500"
-              : "bg-gray-600 text-gray-300 cursor-not-allowed"
-          }`}
-        >
-          CONFIRMAR
-        </button>
-          
-        </div>   
+        </div>
       </div>
-    </div>
+   
   );
 }
--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -7,17 +7,26 @@ export default function Quiz() {
   const name = location.state?.name || 'bruxo misterioso';
 
   return (
-    <div className="min-h-screen flex items-center justify-center relative pb-4">
+    <div className="min-h-screen flex items-start justify-start p-8 relative">
+
       <div className="absolute inset-0 bg-gradient-to-b from-black/70 to-black/90 z-0" />
       <div
         className="absolute inset-0 bg-cover bg-center z-[-1]"
         style={{ backgroundImage: "url('/images/escolahp.jpg')" }}
       />
 
-      <div className="text-center bg-amber-500 z-10 px-4">
-        <h1 className="text-4xl font-bold mb-4">Bem-vindo, {name}!</h1>
-        <p className="mb-6">Prepare-se para o quiz mágico ✨</p>
-        <QuizCard />
+
+      <div className="z-10 w-full max-w-4xl">
+
+        <div className="mb-6 text-white">
+          <h1 className="text-4xl font-bold mb-2">Bem-vindo, {name}!</h1>
+          <p className="text-lg">Prepare-se para o quiz mágico ✨</p>
+        </div>
+
+
+        <div className="w-full max-w-xl">
+          <QuizCard />
+        </div>
       </div>
     </div>
   );
