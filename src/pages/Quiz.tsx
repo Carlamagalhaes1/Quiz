c1573a4283fc0a8f1ddb5fcd347010eb31f70b19
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-11 22:06:45 -0300

    AGORA SIM RESPOSIVIDADE OK

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -17,9 +17,9 @@ export default function Quiz() {
       <div className="mt-4 z-10 max-w-5xl">
 
 
-        <div className="ps-4  sm:ps-12 pe-3.5">
+        <div className=" mt-2 sm:ps-12 pe-3.5 ps-4">
           <div className="text-white mb-6">
-            <h1 className="text-3xl ps-1 sm:text-4xl font-bold mb-1">Bem-vindo, {name}!</h1>
+            <h1 className="text-3xl ps-1 sm:text-4xl font-bold ">Bem-vindo, {name}!</h1>
             <p className="text-lg ps-2 ">Prepare-se para o quiz mágico ✨</p>
           </div>
           <QuizCard />
